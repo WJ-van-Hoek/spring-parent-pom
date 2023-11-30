3138758aa08019682867a47678bc14151544340f
Willem_Windows <wimjoost@gmail.com>
2023-11-30 21:13:28 +0100

    SPP-1: authenticate in mvn version

--- .circleci/bash-increment-version.bash
+++ .circleci/bash-increment-version.bash
@@ -2,7 +2,7 @@
 
 CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 NEXT_VERSION=$(echo "${CURRENT_VERSION}" | awk -F'.' '{print $1"."$2"."$3 + 1}')
-mvn versions:set -DnewVersion="${NEXT_VERSION}"
+mvn --settings .circleci/settings.xml versions:set -DnewVersion="${NEXT_VERSION}"
 
 git config --global user.email "${USER_EMAIL}"
 git config --global user.name "${USER_NAME}"
